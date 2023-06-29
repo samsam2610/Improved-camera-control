c2493e124e5d0c368f17b4ab9b79596ff3a33938
Sam Tran <quocsam93@gmail.com>
2023-06-28 19:33:56 -0500

    debugging

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -515,8 +515,8 @@ class CamGUI(object):
                         
                         next_frame = max(next_frame + 1.0/fps, self.frame_times[num][-1] + 0.5/fps)
             except Exception as e:
-                print(f"Traceback: \n {traceback.format_exc()}")
-        
+                print("Exception occurred:", type(e).__name__, "| Exception value:", e, "| Thread ID:", num, "| Frame count:", self.frame_count[num], "| Capture time:", current_time "| Traceback:", ''.join(traceback.format_tb(e.__traceback__))) 
+  
     def calibrate_on_thread(self):
         frame_groups = {}  # Dictionary to store frame groups by thread_id
         frame_counts = {}  # array to store frame counts for each thread_id
@@ -572,7 +572,7 @@ class CamGUI(object):
                         frame_groups = []
                         frame_count = []
             except Exception as e:
-                print("Exception occurred:", type(e).__name__, "| Exception value:", e, "| Frame:", frame, "| Thread ID:", thread_id, "| Frame count:", frame_count, "| Capture time:", capture_time, "| Traceback:", ''.join(traceback.format_tb(e.__traceback__)))
+                print("Exception occurred:", type(e).__name__, "| Exception value:", e, "| Thread ID:", thread_id, "| Frame count:", frame_count, "| Capture time:", capture_time, "| Traceback:", ''.join(traceback.format_tb(e.__traceback__)))
             
 
     def start_record(self):
