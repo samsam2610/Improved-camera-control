a814d0bbb30fcd04d51b8069ab3904fae93946ff
Sam Tran <quocsam93@gmail.com>
2023-07-06 14:52:00 -0500

    Bug fixes and add barrier to record_on_thread

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -399,9 +399,9 @@ class CamGUI(object):
                 self.cam_name_no_space.append(self.cam_name[num].replace(' ', ''))
                 self.base_name.append(self.cam_name_no_space[num] + '_' +
                                       subject_name + '_' +
-                                      int(da_fps) + 'f' +
-                                      int(temp_exposure) + 'e' +
-                                      int(temp_gain) + 'g')
+                                      str(int(da_fps)) + 'f' +
+                                      str(int(temp_exposure)) + 'e' +
+                                      str(int(temp_gain)) + 'g')
                 self.vid_file.append(os.path.normpath(self.dir_output.get() +
                                                       '/' +
                                                       self.base_name[num] +
