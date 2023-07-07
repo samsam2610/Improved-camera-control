f4fe3acc857b5fd84040548b2f3c243c60054309
Sam Tran <quocsam93@gmail.com>
2023-07-07 14:06:14 -0500

    Adjust frame rate function for better stability
    The camera control functionality was updated to include a stop/start sequence during frame rate modifications. This change ensures the stability of the video capture and prevents potential crashes in the frame grabbing process by buffering the transition between frame rates.

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -362,8 +362,8 @@ class CamGUI(object):
             selected_frame_rate = self.framerate_list[num].get()
         else:
             selected_frame_rate = framerate
-        self.cam[num].set_frame_rate(int(selected_frame_rate))
-        result = self.framerate[num].set(selected_frame_rate)
+        result = self.cam[num].set_frame_rate(int(selected_frame_rate))
+        self.framerate[num].set(selected_frame_rate)
         current_framerate = self.get_current_frame_rate(num)
         print(f'Selected: {selected_frame_rate }. Frame rate set to {current_framerate} fps. Result: {result}')
         
