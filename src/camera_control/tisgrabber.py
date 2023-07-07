12c80a2c5b99692c66852f7e2c6fc77252a25519
Sam Tran <quocsam93@gmail.com>
2023-07-07 14:00:22 -0500

    Add new button to display captured videos stats

--- src/camera_control/tisgrabber.py
+++ src/camera_control/tisgrabber.py
@@ -598,7 +598,8 @@ class TIS_CAM(object):
             return TIS_GrabberDLL.set_videoformat(self._handle, self.s(Format))
 
         def SetFrameRate(self, FPS):
-            return TIS_GrabberDLL.set_framerate(self._handle, FPS)
+            result = TIS_GrabberDLL.set_framerate(self._handle, FPS)
+            return result.value
         
         def GetFrameRate(self):
             return TIS_GrabberDLL.get_framerate(self._handle)
