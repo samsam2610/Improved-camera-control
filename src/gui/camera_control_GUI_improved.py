c2f3627a9f8dcd4fa334a2e7e5ee810fc302ddc9
Sam Tran <quocsam93@gmail.com>
2023-07-13 18:45:16 -0500

    Disable live calibration and re-enable marker drawing
    This change comments out the call to 'show_calibration_live' from testscript_GUI.py. The reason is to disable the live calibration during the testing phase, thus reducing computational load.
    In camera_control_GUI_improved.py, the function to draw detected AruCo markers using 'rejected_points' is re-enabled, which was previously commented out. This step is taken to aid in debugging and marker tracking.

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -1378,7 +1378,7 @@ class CamGUI(object):
 
             cv2.aruco.drawDetectedCornersCharuco(frame, reshape_corners, c_ids)
             cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-            # cv2.aruco.drawDetectedMarkers(frame, rejected_points, borderColor=(100, 0, 240))
+            cv2.aruco.drawDetectedMarkers(frame, rejected_points, borderColor=(100, 0, 240))
 
         except cv2.error as e:
             import sys
