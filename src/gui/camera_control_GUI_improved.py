138f7e9384c63a2c45daa2bb57085ae3a3ac6db1
Sam Tran <quocsam93@gmail.com>
2023-07-13 13:33:38 -0500

    Fix timing barrier implementation in GUI camera control
    The placement of the barrier.wait() command was updated to fix its trigger timing within the fps calculation. The previous location outside the loop resulted in improper synchronization across threads, by timely adjusting its position within the next_frame calculation, the synchronization issue has been addressed.

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -922,7 +922,6 @@ class CamGUI(object):
                                           self.frame_times[num][-1]))  # captured time
 
                     next_frame = max(next_frame + 1.0/fps, self.frame_times[num][-1] + 0.5/fps)
-                    barrier.wait()
             
             if (time.perf_counter() - start_time) > self.calibration_duration or self.calibration_capture_toggle_status:
                 print(f"Calibration capture on cam {num}: duration exceeded or toggle status is True")
