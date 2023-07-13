6e02972bc412dc1e7103c7bc6a352c80b97a1b35
Sam Tran <quocsam93@gmail.com>
2023-07-13 17:34:05 -0500

    Reorganize GUI placeholders in camera control code
    Updated the structure of the camera control GUI code for better readability and code organization. Previously, GUI placeholders were scattered throughout the code in various functions. In this diff, they have been moved and grouped under their own descriptive section after the initial class variable declarations. This organization simplifies future code modifications or enhancements.

--- src/gui/testscript_GUI.py
+++ src/gui/testscript_GUI.py
@@ -4,14 +4,15 @@ class CamGUI_Tests(CamGUI):
     def __init__(self, debug_mode=False, init_cam_bool=True):
         super().__init__(debug_mode, init_cam_bool)
         
+    def init_cams(self):
+        for i, button in enumerate(self.cam_init_button):
+            time.sleep(1)
+            button.invoke()
+
     def auto_init_cam(self):
         print("Initialize camera in test mode")
-        delay = 1000  # Delay between invocations in milliseconds
-       
-        time.sleep(2)
-        for i, button in enumerate(self.cam_init_button):
-            self.window.after(delay * i, button.invoke)
         
+        self.window.after(1000, self.init_cams)
         self.window.mainloop()
  
 
