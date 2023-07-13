adced1ef01d15870b798ab9d73c3c13f311f4864
Sam Tran <quocsam93@gmail.com>
2023-07-13 17:17:58 -0500

    Refactor auto camera initialization to improve delays
    Auto-initialization for the camera interface used to be done synchronously, resulting in unnecessary waits and potential issues. This update migrates the initialization to a more asynchronous approach, using time delay loops for handling multiple camera initialization commands. The code for auto-initialization has also been encapsulated into a CamGUI_auto class for better organization, modularity, and testing. The change was done in both testscript_GUI.py and camera_control_GUI_improved.py files.

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -2014,7 +2014,7 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--debug", action="store_true", dest='debug_mode', help="Enable debug mode")
     parser.add_argument("-ni", "--no-init-cam", action="store_false", dest="init_cam_bool",
                         help="Disable camera initialization")
-    parser.add_argument("-t", "--test", action="store_true", dest="test_mode", help="Enable test mode")
+    parser.add_argument("-t", "--test", action="store_false", dest="test_mode", help="Enable test mode")
 
     # Parse the command-line arguments
     args = parser.parse_args()
