98421a0ea08d00b26cec0ef707aa89d1d84a881d
Sam Tran <quocsam93@gmail.com>
2023-07-13 14:31:34 -0500

    "Refactor calibration settings load method in camera GUI
    Moved the loading of calibration settings into its own separate method in the camera control GUI. This change quickens the code development process by easing potential future changes to the calibration process. It also increases readability and maintainability of the code and keeps the setup_calibration function more concise and focused on its primary task. The changes should not affect the functionality of the GUI."

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -1314,6 +1314,7 @@ class CamGUI(object):
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
+            traceback.print_exc()
             return None
 
         if verbose:
