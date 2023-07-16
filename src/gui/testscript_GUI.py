c5e3bd1c691b9e384fcd37f28cb3d77fca849e1c
Sam Tran <quocsam93@gmail.com>
2023-07-16 14:52:23 -0500

    Add frame ready callback and flip feature in camera control
    This commit introduces a callback function when the frame is ready in the 'disable_trigger' method of 'camera_control'. This will help us manage resources more efficiently by not blocking the execution while waiting for the frame. Additionally, a new feature to flip the image vertically has been added to correct the orientation of the captured frames. The code for creating the numpy array from the byte buffer was also optimized to be more readable and efficient.

--- src/gui/testscript_GUI.py
+++ src/gui/testscript_GUI.py
@@ -16,14 +16,17 @@ class CamGUI_Tests(CamGUI):
         # self.test_calibration_live_button.invoke()
        
     def test_experimental_func(self):
+        self.init_cams()
         time.sleep(1)
+        self.show_calibration_live()
+        time.sleep(3)
         self.setup_trigger_recording_button.invoke()
         
     def auto_init_cam(self):
         print("Initialize camera in test mode")
         
-        self.window.after(2000, self.init_cams)
-        self.window.after(3000, self.show_calibration_live)
+        # self.window.after(2000, self.init_cams)
+        # self.window.after(3000, self.show_calibration_live)
         self.window.after(3000, self.test_experimental_func)
         self.window.mainloop()
  
