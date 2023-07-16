9a498c1d86fa32678b53520feecc2f9b3e38613e
Sam Tran <quocsam93@gmail.com>
2023-07-16 16:33:48 -0500

    Add frame ready callback and flip feature in camera control
    This commit introduces a callback function when the frame is ready in the 'disable_trigger' method of 'camera_control'. This will help us manage resources more efficiently by not blocking the execution while waiting for the frame. Additionally, a new feature to flip the image vertically has been added to correct the orientation of the captured frames. The code for creating the numpy array from the byte buffer was also optimized to be more readable and efficient.

--- src/camera_control/ic_camera.py
+++ src/camera_control/ic_camera.py
@@ -196,9 +196,10 @@ class ICCam(ctypes.Structure):
     
     def release_video_file(self):
         if self.vid_file is not None:
-            self.vid_file.release()
             frame_times = copy.deepcopy(self.vid_file.frame_times)
             frame_num = copy.deepcopy(self.vid_file.frame_num)
+            self.vid_file.release()
+            
             print(f'Flipping vertical back for cam {self.cam_num}')
             self.cam.SetPropertySwitch("Flip Vertical", "Enable", False)
             
