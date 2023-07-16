723e7c9fe292c9c715f09f8f9c67834d4d25ad97
Sam Tran <quocsam93@gmail.com>
2023-07-15 20:39:27 -0500

    Improve thread handling for camera recording
    This commit refactors the camera recording and trigger process to improve its thread handling. Previously, threads may not sync optimally when enabling and disabling the camera triggers, which could lead to inefficient behavior or potential race conditions. The code now uses a `threading.Barrier` to ensure that all camera threads start simultaneously. It also maintains a record of all active recording threads so it can wait for all of them to finish before processing the final frame, ensuring no frames are missed. The boolean flag `recording_trigger_toggle_status` was added to control the trigger state more finely. This enhancement leads to a smoother recording process and reduces the risk of potential issues related to threading.

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -1108,9 +1108,7 @@ class CamGUI(object):
             subject_name = self.subject.get() + '_' + date + '_' + self.attempt.get()
             create_output_files(self, subject_name=subject_name)
             
-            # Set frame callback first
-            for num in range(len(self.cam)):
-                self.cam[num].set_frame_callback_video()
+            
  
             self.recording_trigger_toggle_status = True
             self.setup = True
@@ -1159,6 +1157,9 @@ class CamGUI(object):
             self.recording_trigger_thread = []
             self.recording_trigger_status = [True for i in range(len(self.cam))]
             self.recording_trigger_toggle_status = True
+            # Set frame callback first
+            for num in range(len(self.cam)):
+                self.cam[num].set_frame_callback_video()
             # enable the trigger
             for i in range(len(self.cam)):
                 thread_name = f"Cam {i + 1} thread"
@@ -1758,7 +1759,7 @@ class CamGUI(object):
         
         self.toggle_trigger_recording_status = IntVar(value=0)
         self.toggle_trigger_recording_button = Button(experimental_functions_frame, text="Capture Disabled",
-                                                    background="red", state="disabled", width=14, command=self.toggle_trigger_recording)
+                                                    background="red", state="enabled", width=14, command=self.toggle_trigger_recording)
         self.toggle_trigger_recording_button.grid(sticky="nsew", row=0, column=1, padx=5, pady=3)
         Hovertip(self.toggle_trigger_recording_button, "Start/Stop listening to trigger to capture frame")
         
