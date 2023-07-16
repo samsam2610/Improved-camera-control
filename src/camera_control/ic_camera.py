6dd32b714f5a47129ca3af988e8f97416452292c
Sam Tran <quocsam93@gmail.com>
2023-07-15 22:16:19 -0500

    "Improve threading in camera GUI "
    Refactored the way threads are created and managed in the camera GUI. Implemented functionality where threads are first setup for setting frame callbacks for each camera before enabling the trigger. This improves the thread synchronization and ensures that all threads complete their tasks efficiently. The change will improve the performance of the camera control and enhance the user experience.

--- src/camera_control/ic_camera.py
+++ src/camera_control/ic_camera.py
@@ -283,6 +283,6 @@ class VideoRecordingSession(ctypes.Structure):
         self.frame_num = []
         
     def write(self, frame, time_data, frame_num):
-        self.vid_out.write(frame)
+        self.vid_out.write(cv2.flip(frame, 0))
         self.frame_times.append(time_data)
         self.frame_num.append(frame_num)
