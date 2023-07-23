9a1a618e23ac4412312f02b02bfb1bf2b5ff30b0
Sam Tran <quocsam93@gmail.com>
2023-07-23 12:07:18 -0500

    "Modify padding and row updating in UI elements layout"
    Adjusted padding in the grid layout of camera_status_frame labels and record_video_frame. The vertical padding was removed to decrease space between elements for a condensed UI. Also, the row updating technique was modified to ensure proper assignment and increment of cur_row variable for smoother transition and neat arrangement of frames and labels within the grid system.

--- src/gui/camera_control_GUI_improved.py
+++ src/gui/camera_control_GUI_improved.py
@@ -1635,10 +1635,10 @@ class CamGUI(object):
             cur_row += 1
 
             # empty row
-            Label(self.window, text="").grid(row=cur_row + 2, column=0)
+            Label(self.window, text="").grid(row=cur_row + 1, column=0)
 
             # end of camera loop
-            cur_row = cur_row + 3
+            cur_row = cur_row + 2
 
         video_setting_label = Label(self.window, text="Video Settings: ", font=("Arial", 12, "bold"))
         video_setting_label.grid(row=cur_row, column=0, padx=1, pady=1, sticky="w")
@@ -1786,8 +1786,8 @@ class CamGUI(object):
         
         cur_row += 1
         # empty row
-        Label(self.window, text="").grid(row=cur_row + 2, column=0)
-        cur_row += 3
+        Label(self.window, text="").grid(row=cur_row + 1, column=0)
+        cur_row += 2
 
         # Experimental settings
         experimental_settings_label = Label(self.window, text="Experimental settings: ", font=("Arial", 12, "bold"))
@@ -1838,8 +1838,8 @@ class CamGUI(object):
         
         cur_row += 1
         # empty row
-        Label(self.window, text="").grid(row=cur_row + 2, column=0)
-        cur_row += 3
+        Label(self.window, text="").grid(row=cur_row + 1, column=0)
+        cur_row += 2
         
         ## calibrate video section
         calibration_label = Label(self.window, text="Calibration: ", font=("Arial", 12, "bold"))
