5c287211f22b452dae23cf076700d6ee78d34752
Sam Tran <quocsam93@gmail.com>
2023-07-27 20:21:40 -0500

    Fixed byte string parameters in camera ROI settings
    This commit changes the arguments passed to the SetPropertyAbsoluteValue function from string to byte string. The former method was causing compatibility issues as the library used (likely for interacting with the hardware camera) was expecting byte string arguments instead. This led to unwanted and incorrect behavior whenever the ROI (Region of Interest) of the camera was set. By converting the arguments to byte string, this problem is resolved and the function operates as expected.

--- src/camera_control/ic_camera.py
+++ src/camera_control/ic_camera.py
@@ -70,10 +70,10 @@ class ICCam(ctypes.Structure):
 
     def set_ROI(self):
         # result_roi = self.cam.SetPropertySwitch("Auto Functions ROI", "Enabled", True)
-        result_left = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", "Left", int(self.crop['left']))
-        result_right = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", "Top", int(self.crop['top']))
-        result_width = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", "Width", int(self.crop['width']))
-        result_height = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", "Height", int(self.crop['height']))
+        result_left = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", b"Left", int(self.crop['left']))
+        result_right = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", b"Top", int(self.crop['top']))
+        result_width = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", b"Width", int(self.crop['width']))
+        result_height = self.cam.SetPropertyAbsoluteValue("Auto Functions ROI", b"Height", int(self.crop['height']))
         print(f'For cam {self.cam_num}, the ROI was set with results: {result_left}, {result_right}, {result_width}, {result_height}')
         
     def set_crop(self, top=None, left=None, height=None, width=None):
