bc3e2b1035de5e095cf90b8dfdba4c6d65a43bb6
Sam Tran <quocsam93@gmail.com>
2023-08-05 10:54:44 -0500

    Include toggle into calibration capture

--- src/gui/_video_files_func.py
+++ src/gui/_video_files_func.py
@@ -132,7 +132,7 @@ def save_vid(self, compress=False, delete=False, plotData=False):
     self.vid_out = []
     self.frame_times = []
     # self.current_file_label['text'] = ""
-    self.received_pulse_label['text'] = ""
+    # self.received_pulse_label['text'] = ""
     self.set_calibration_buttons_group(state='disabled')
 
 
