462bc54157d75ca0867a1c5f9dd620b04aaf189d
Sam Tran <quocsam93@gmail.com>
2024-04-29 16:19:02 -0500

    Test with changing sleep time while waiting to write frame

--- src/camera_control/ic_camera.py
+++ src/camera_control/ic_camera.py
@@ -587,8 +587,9 @@ class VideoRecordingSession(ctypes.Structure):
             #         self.write_frame()
             #         return -1
             time.sleep(0.00005)
-        
+        print(f'Cam {self.cam_num} thread is stopping, current frame count: {self.frame_count}')
         self.write_frame()  # write the last frame
+        print(f'Cam {self.cam_num} thread is stopped, current frame count: {self.frame_count}')
 
 
 class FrameData(ctypes.Structure):
